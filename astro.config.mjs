39e63d6c118c49fade07d2970b44b7196a7cc025
Diego Paez <diego@geutstudio.com>
2026-04-07 23:46:31 -0300

    Update: prepublish tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,5 +45,6 @@ export default defineConfig({
 
   adapter: cloudflare({
     sessionKVBindingName: "DK_SPOT_SESSIONS",
+    imageService: { build: 'compile', runtime: 'cloudflare-binding' }
   })
 });
